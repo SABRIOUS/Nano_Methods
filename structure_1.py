196e024c50d30dc8053246ad2d22f76987390b86
SABRIOUS <ahmedsabrious@gmail.com>
2020-08-07 20:49:59 +0200

    First Chapter Solutions data ST&ALG

---
+++ structure_1.py
@@ -0,0 +1,16 @@
+# 51
+# Write a short Python function, is multiple(n, m),
+# that takes two integer
+# values and returns True if n is a multiple of m,
+# that is, n = mi for some
+# integer i, and False otherwise.
+print('-'*70+'\n')
+# def is_multible(n,m):
+#     try:
+#         if m%n ==0:
+#             return True
+#         return False
+#     except:
+#         return "Not a number or divided by zero"
+#
+# print(is_multible(5,6))
